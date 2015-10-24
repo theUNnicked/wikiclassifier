9a423409e30d9722422e0aae55654c26c3f89a58
Wojtek Stanislawski <stanik.woj@gmail.com>
2015-10-24 20:22:03 +0200

    Poprawka skryptu runHelper.sh, do testow

--- SourceCode/kaw/runHelper.sh
+++ SourceCode/kaw/runHelper.sh
@@ -1,2 +1,2 @@
-export WIKIDUMPS_FILE=
-java -jar target\kaw-0.0.1-SNAPSHOT-jar-with-dependencies.jar --dump $WIKIDUMPS_FILE
+export WIKIDUMPS_FILE=/home/wojtek/Pobrane/plwiki-20110308-pages-articles.xml
+java -jar ./target/kaw-0.0.1-SNAPSHOT-jar-with-dependencies.jar --dump $WIKIDUMPS_FILE
